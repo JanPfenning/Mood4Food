de3d6a6d01db1ece3b2796a412820af590b5f41d
JanPfenning <jan.pfenning@gmail.com>
2020-11-03 15:19:18 +0100

    add blank pages and nav from home

--- app/src/main/java/com/jrk/mood4food/MainActivity.kt
+++ app/src/main/java/com/jrk/mood4food/MainActivity.kt
@@ -1,10 +1,39 @@
 package com.jrk.mood4food
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        //Navigate to Waterbalance UI
+        findViewById<LinearLayout>(R.id.water_nav_bar).setOnClickListener{
+            setContentView(R.layout.activity_water_balance)
+        }
+
+        //Navigate to Nutrietns UI
+        findViewById<LinearLayout>(R.id.nutrients).setOnClickListener{
+            setContentView(R.layout.activity_nutrients)
+        }
+
+        //Navigate to Settings UI
+        findViewById<LinearLayout>(R.id.settings).setOnClickListener{
+            setContentView(R.layout.activity_settings)
+        }
+
+        //Navigate to Recipes UI
+        findViewById<LinearLayout>(R.id.recipes).setOnClickListener{
+            setContentView(R.layout.activity_recipes)
+        }
+
+        //Navigate to Home UI
+        findViewById<LinearLayout>(R.id.home).setOnClickListener{
+            setContentView(R.layout.activity_home)
+        }
+
     }
 }
