63fffdd2b38cdcc322e8f93b38c700096ad6fae1
Paul-Finch <konrad.schewe@outlook.de>
2020-11-03 11:50:18 +0100

    Fixed Errors

---
+++ app/src/main/java/com/jrk/mood4food/WaterBalanceActivity.kt
@@ -0,0 +1,22 @@
+package com.jrk.mood4food
+
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
+import org.xmlpull.v1.XmlPullParser
+
+class WaterBalanceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_water_balance)
+
+    }
+
+}
