e821aeb6e0b4ebf26b3fef90eb10626eb848aeeb
Rico  Rauschkolb <r.rauschkolb@web.de>
2020-11-08 16:28:15 +0100

    water add

--- app/src/main/java/com/jrk/mood4food/WaterBalanceActivity.kt
+++ app/src/main/java/com/jrk/mood4food/WaterBalanceActivity.kt
@@ -1,21 +1,29 @@
 package com.jrk.mood4food
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.text.InputType
 import android.util.Log
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
+import com.john.waveview.WaveView
 
 
 class WaterBalanceActivity : AppCompatActivity() {
-
+    private val sharedPrefFile = "mood4Food"
+    private var  waterlevel:Float = 0.0F;
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_water_balance)
+        val sharedPref: SharedPreferences = getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        waterlevel = getWaterlevelP();
+        update();
         findViewById<AppCompatImageView>(R.id.addwater).setOnClickListener{
             val builder1 = AlertDialog.Builder(this)
             builder1.setMessage("Wie viel hast du getrunken")
@@ -26,8 +34,16 @@ class WaterBalanceActivity : AppCompatActivity() {
             builder1.setPositiveButton(
 
                     "Add", DialogInterface.OnClickListener { dialog, id ->
-                    val inputText = input.text.toString().toDouble()
-                      
+
+                val inputText = input.text.toString().toFloat()
+                waterlevel += inputText.toFloat()
+                update()
+                setWaterlevelP(waterlevel)
+
+
+
+
+
             })
 
 
@@ -40,5 +56,26 @@ class WaterBalanceActivity : AppCompatActivity() {
         }
 
     }
+    fun update(){
+
+        val progress = (waterlevel)* (100/3)
+        findViewById<WaveView>(R.id.waveView).setProgress(progress.toInt())
+        findViewById<TextView>(R.id.waterlevelPercentage).setText(progress.toInt().toString()+ " %")
+
+    }
+    fun getWaterlevelP():Float{
+        val sharedPref: SharedPreferences = getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val waterlevel =   sharedPref.getFloat("waterLevel", 0.0F)
+        return  waterlevel
+
+
+    }
+    fun setWaterlevelP(level:Float){
+        val sharedPref: SharedPreferences = getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putFloat("waterLevel", level)
+        editor.commit()
+    }
+
 
 }
