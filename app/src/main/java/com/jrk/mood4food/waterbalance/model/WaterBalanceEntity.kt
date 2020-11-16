5d333f12fcd91c888021bedf0675a778f71d787c
Rico  Rauschkolb <r.rauschkolb@web.de>
2020-11-16 21:55:16 +0100

    V1.0 MVC WaterBalance Storage -> null value or 0

--- app/src/main/java/com/jrk/mood4food/waterbalance/model/WaterBalanceEntity.kt
+++ app/src/main/java/com/jrk/mood4food/waterbalance/model/WaterBalanceEntity.kt
@@ -7,6 +7,9 @@ import java.util.*
 class WaterBalanceEntity(context: Context) : LocalEntity(context, WaterBalanceEntity::class.java, true) {
 
     var waterBalance = 0f
+    var currentDate = "--"
+
+
 
 
 
