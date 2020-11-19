111dc2b0b31724fa232413d4d125515ab0519f87
JanPfenning <jan.pfenning@gmail.com>
2020-11-19 22:20:13 +0100

    trying to make sure an ingredient has a maximum of 2 values

--- app/src/main/java/com/jrk/mood4food/Tuple.kt
+++ app/src/main/java/com/jrk/mood4food/Tuple.kt
@@ -1,2 +1,21 @@
 package com.jrk.mood4food
 
+class Tuple<T>(override val size: Int = 2) :Set<T>{
+    private lateinit var tuple : Set<T>
+
+    override fun contains(element: T): Boolean {
+        return tuple.contains(element)
+    }
+
+    override fun containsAll(elements: Collection<T>): Boolean {
+        return tuple.containsAll(elements)
+    }
+
+    override fun isEmpty(): Boolean {
+        return tuple.isEmpty()
+    }
+
+    override fun iterator(): Iterator<T> {
+        return tuple.iterator()
+    }
+}
