cbc5040f9d0fd2fb7c5494914212bfe12540a225
JanPfenning <jan.pfenning@gmail.com>
2020-11-27 13:53:52 +0100

    add onclick listener to search galery for fotos

--- app/src/main/java/com/jrk/mood4food/recipes/add_mod/view/Add_ModActivity.kt
+++ app/src/main/java/com/jrk/mood4food/recipes/add_mod/view/Add_ModActivity.kt
@@ -1,12 +1,13 @@
 package com.jrk.mood4food.recipes.add_mod.view
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.jrk.mood4food.*
 import com.jrk.mood4food.model.ModelModule
@@ -38,6 +39,24 @@ class Add_ModActivity : AppCompatActivity(), Add_ModView, Add_ModObserver {
 
         findViewById<TextView>(R.id.upload_picture).setOnClickListener{
             //TODO on clicking upload picture
+            //check runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                        PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE);
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            }
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
         }
 
         findViewById<ImageView>(R.id.cancel_modify_recipe).setOnClickListener{
@@ -69,6 +88,21 @@ class Add_ModActivity : AppCompatActivity(), Add_ModView, Add_ModObserver {
         }
     }
 
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+        //Permission code
+        private val PERMISSION_CODE = 1001;
+    }
+
+
     override fun onStart() {
         super.onStart()
         model.register(this)
@@ -85,6 +119,31 @@ class Add_ModActivity : AppCompatActivity(), Add_ModView, Add_ModObserver {
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra("id",recipeEntity.storageAddress)
         startActivity(intent)
+    }
+
+    //handle requested permission result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size >0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    pickImageFromGallery()
+                }
+                else{
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            findViewById<ImageView>(R.id.imageView).setImageURI(data?.data)
+        }
     }
 
 }
