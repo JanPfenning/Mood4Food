c9f88cd6498e8393070bc8665bed2f3ee3dba553
Paul-Finch <konrad.schewe@outlook.de>
2020-12-02 20:35:13 +0100

    ListView shows it's whole content by customizing ListView with new StretchingListView class

---
+++ app/src/main/java/com/jrk/mood4food/components/StretchingListView.kt
@@ -0,0 +1,17 @@
+package com.jrk.mood4food.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ListView
+
+class StretchingListView : ListView {
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {}
+
+    public override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val expandSpec = MeasureSpec.makeMeasureSpec(Int.MAX_VALUE shr 2,
+                MeasureSpec.AT_MOST)
+        super.onMeasure(widthMeasureSpec, expandSpec)
+    }
+}
