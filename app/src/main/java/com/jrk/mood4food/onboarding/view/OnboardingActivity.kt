f589b619f3ef64b71b9c1738346df788b80f9bd9
Paul-Finch <konrad.schewe@outlook.de>
2021-05-19 10:14:32 +0200

    Onboarding pages are swipeable now

--- app/src/main/java/com/jrk/mood4food/onboarding/view/OnboardingActivity.kt
+++ app/src/main/java/com/jrk/mood4food/onboarding/view/OnboardingActivity.kt
@@ -1,15 +1,18 @@
 package com.jrk.mood4food.onboarding.view
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.jrk.mood4food.App
 import com.jrk.mood4food.R
 import com.jrk.mood4food.home.view.MainActivity
 
-class OnboardingActivity : AppCompatActivity() {
+class OnboardingActivity() : AppCompatActivity() {
 
     var pageIndex = -1
     private val pages = intArrayOf(
@@ -26,22 +29,43 @@ class OnboardingActivity : AppCompatActivity() {
         nextPage()
     }
 
+    private fun lastPage(){
+        if(pageIndex - 1 > -1) {
+            pageIndex--
+            initPage()
+        }
+    }
+
     private fun nextPage(){
         pageIndex++
         if(pageIndex < pages.size) {
-            setContentView(pages[pageIndex])
-
-            val skipBtn = findViewById<Button>(R.id.skip_btn)
-            skipBtn.setOnClickListener(View.OnClickListener {
-                finishOnboarding()
-            })
-            val nextBtn = findViewById<LinearLayout>(R.id.next_btn)
-            nextBtn.setOnClickListener(View.OnClickListener {
-                nextPage()
-            })
+            initPage()
         }else{
             finishOnboarding()
         }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initPage(){
+        setContentView(pages[pageIndex])
+
+        val skipBtn = findViewById<Button>(R.id.skip_btn)
+        skipBtn.setOnClickListener(View.OnClickListener {
+            finishOnboarding()
+        })
+        val nextBtn = findViewById<LinearLayout>(R.id.next_btn)
+        nextBtn.setOnClickListener(View.OnClickListener {
+            nextPage()
+        })
+        val layout = findViewById<ConstraintLayout>(R.id.onboarding_layout)
+        layout.setOnTouchListener(object: OnSwipeTouchListener(App.getContext()) {
+            override fun onSwipeLeft() {
+                nextPage()
+            }
+            override fun onSwipeRight() {
+                lastPage()
+            }
+        })
     }
 
     private fun finishOnboarding(){
