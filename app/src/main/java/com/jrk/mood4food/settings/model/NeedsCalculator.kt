ece8fe62f3d6532adb915466880edb57c3a32cf0
Rico  Rauschkolb <r.rauschkolb@web.de>
2021-05-28 12:10:44 +0200

    NeedsCalculator.kt outsource from Repository

---
+++ app/src/main/java/com/jrk/mood4food/settings/model/NeedsCalculator.kt
@@ -0,0 +1,53 @@
+package com.jrk.mood4food.settings.model
+
+import android.util.Log
+import com.jrk.mood4food.settings.PhysicalActivity
+import com.jrk.mood4food.waterbalance.model.SettingsEntity
+import kotlin.math.roundToInt
+
+object NeedsCalculator {
+    fun calcCarbohydratePerDay(calculationData: SettingsEntity): Int {
+        var proportion = calculationData.caloriesPerDay * 0.55
+        return (proportion * (1 / 4.1)).toInt()
+
+    }
+
+    fun calcFatPerDay(calculationData: SettingsEntity): Int {
+        var proportion = calculationData.caloriesPerDay * 0.30
+        return (proportion * (1 / 9.3)).toInt()
+
+    }
+
+    fun calcProteinPerDay(calculationData: SettingsEntity): Int {
+        var proportion = calculationData.caloriesPerDay * 0.15
+        return (proportion * (1 / 4.1)).toInt()
+    }
+
+
+    fun calcCaloriesPerDay(calculationData: SettingsEntity): Int {
+        var caloriesPerDay = 0
+        if (calculationData.gender == "Male") {
+            caloriesPerDay = ((13.7 * calculationData.currentBodyWeight) + (5 * calculationData.bodySize) - (6.8 * calculationData.age)).toInt()
+        }
+        if (calculationData.gender == "Female")
+            caloriesPerDay = ((655.1 + (9.6 * calculationData.currentBodyWeight) + (1.8 * calculationData.currentBodyWeight) - (4.7 * calculationData.age))).toInt()
+        Log.i("test", calculationData.physicalActivity)
+
+        var factor: Float = when (PhysicalActivity.getByText(calculationData.physicalActivity)) {
+            PhysicalActivity.Lowest -> 1.2F
+            PhysicalActivity.Low -> 1.375F
+            PhysicalActivity.Middle -> 1.55F
+            PhysicalActivity.High -> 1.725F
+            PhysicalActivity.Highest -> 1.9F
+            else -> {
+                1F
+            }
+        }
+
+        return (caloriesPerDay * factor).toInt()
+    }
+
+    fun calcWaterPerDay(calculationData: SettingsEntity): Float {
+        return ((((calculationData.currentBodyWeight * calculationData.age) / 28.3F) * 0.03F) * 10.0).roundToInt() / 10.0F
+    }
+}
--- app/src/main/java/com/jrk/mood4food/settings/model/SettingsRepository.kt
+++ app/src/main/java/com/jrk/mood4food/settings/model/SettingsRepository.kt
@@ -1,75 +1,27 @@
 package com.jrk.mood4food.waterbalance.model
 
-import android.util.Log
 import com.jrk.mood4food.App
 import com.jrk.mood4food.model.localStorage.LocalStorageInterface
-import com.jrk.mood4food.settings.PhysicalActivity
+import com.jrk.mood4food.settings.model.NeedsCalculator
 import com.jrk.mood4food.settings.model.SettingsConverter
 import com.jrk.mood4food.settings.view.IngredientSettings
-import kotlin.math.roundToInt
 
 class SettingsRepository(localStorage: LocalStorageInterface) {
     var localStorage = localStorage
     lateinit var tmpEnitity: SettingsEntity
 
     fun calculateNeeds(calculationData: SettingsEntity) {
-        calculationData.waterPerDay = calcWaterPerDay(calculationData)
-        calculationData.caloriesPerDay = calcCaloriesPerDay(calculationData)
-        calculationData.proteinPerDay = calcProteinPerDay(calculationData)
-        calculationData.carbohydratesPerDay = calcCarbohydratePerDay(calculationData)
-        calculationData.fatPerDay = calcFatPerDay(calculationData)
+        calculationData.waterPerDay = NeedsCalculator.calcWaterPerDay(calculationData)
+        calculationData.caloriesPerDay = NeedsCalculator.calcCaloriesPerDay(calculationData)
+        calculationData.proteinPerDay = NeedsCalculator.calcProteinPerDay(calculationData)
+        calculationData.carbohydratesPerDay = NeedsCalculator.calcCarbohydratePerDay(calculationData)
+        calculationData.fatPerDay = NeedsCalculator.calcFatPerDay(calculationData)
         tmpEnitity = calculationData
 
     }
 
     fun storeSettings(calculationData: SettingsEntity) {
         localStorage.save(App.getContext(), calculationData)
-    }
-
-
-    private fun calcCarbohydratePerDay(calculationData: SettingsEntity): Int {
-        var proportion = calculationData.caloriesPerDay * 0.55
-        return (proportion * (1 / 4.1)).toInt()
-
-    }
-
-    private fun calcFatPerDay(calculationData: SettingsEntity): Int {
-        var proportion = calculationData.caloriesPerDay * 0.30
-        return (proportion * (1 / 9.3)).toInt()
-
-    }
-
-    private fun calcProteinPerDay(calculationData: SettingsEntity): Int {
-        var proportion = calculationData.caloriesPerDay * 0.15
-        return (proportion * (1 / 4.1)).toInt()
-    }
-
-
-    private fun calcCaloriesPerDay(calculationData: SettingsEntity): Int {
-        var caloriesPerDay = 0
-        if (calculationData.gender == "Male") {
-            caloriesPerDay = ((13.7 * calculationData.currentBodyWeight) + (5 * calculationData.bodySize) - (6.8 * calculationData.age)).toInt()
-        }
-        if (calculationData.gender == "Female")
-            caloriesPerDay = ((655.1 + (9.6 * calculationData.currentBodyWeight) + (1.8 * calculationData.currentBodyWeight) - (4.7 * calculationData.age))).toInt()
-        Log.i("test", calculationData.physicalActivity)
-
-        var factor: Float = when (PhysicalActivity.getByText(calculationData.physicalActivity)) {
-            PhysicalActivity.Lowest -> 1.2F
-            PhysicalActivity.Low -> 1.375F
-            PhysicalActivity.Middle -> 1.55F
-            PhysicalActivity.High -> 1.725F
-            PhysicalActivity.Highest -> 1.9F
-            else -> {
-                1F
-            }
-        }
-
-        return (caloriesPerDay * factor).toInt()
-    }
-
-    private fun calcWaterPerDay(calculationData: SettingsEntity): Float {
-        return ((((calculationData.currentBodyWeight * calculationData.age) / 28.3F) * 0.03F) * 10.0).roundToInt() / 10.0F
     }
 
     fun getSettings(): SettingsEntity {
@@ -83,7 +35,6 @@ class SettingsRepository(localStorage: LocalStorageInterface) {
         return entity
 
     }
-
 
     fun saveIngredients(ingredientsGood: MutableSet<IngredientSettings>, ingredientsBad: MutableSet<IngredientSettings>) {
         var ingredientsGood = SettingsConverter.ingToMutableSet(ingredientsGood)
@@ -129,18 +80,18 @@ class SettingsRepository(localStorage: LocalStorageInterface) {
 
     fun calculateChangedGoals(changedSettings: SettingsEntity) {
         val currentSettings = getSettings()
-        currentSettings.waterPerDay = calcWaterPerDay(currentSettings)
+        currentSettings.waterPerDay = NeedsCalculator.calcWaterPerDay(currentSettings)
         if (currentSettings.caloriesPerDay != changedSettings.caloriesPerDay) {
             currentSettings.caloriesPerDay = changedSettings.caloriesPerDay
-            currentSettings.proteinPerDay = calcProteinPerDay(changedSettings)
-            currentSettings.carbohydratesPerDay = calcCarbohydratePerDay(changedSettings)
-            currentSettings.fatPerDay = calcFatPerDay(changedSettings)
+            currentSettings.proteinPerDay = NeedsCalculator.calcProteinPerDay(changedSettings)
+            currentSettings.carbohydratesPerDay = NeedsCalculator.calcCarbohydratePerDay(changedSettings)
+            currentSettings.fatPerDay = NeedsCalculator.calcFatPerDay(changedSettings)
         } else if (currentSettings.physicalActivity != changedSettings.physicalActivity) {
             currentSettings.physicalActivity = changedSettings.physicalActivity
-            currentSettings.caloriesPerDay = calcCaloriesPerDay(currentSettings)
-            currentSettings.proteinPerDay = calcProteinPerDay(currentSettings)
-            currentSettings.carbohydratesPerDay = calcCarbohydratePerDay(currentSettings)
-            currentSettings.fatPerDay = calcFatPerDay(currentSettings)
+            currentSettings.caloriesPerDay = NeedsCalculator.calcCaloriesPerDay(currentSettings)
+            currentSettings.proteinPerDay = NeedsCalculator.calcProteinPerDay(currentSettings)
+            currentSettings.carbohydratesPerDay = NeedsCalculator.calcCarbohydratePerDay(currentSettings)
+            currentSettings.fatPerDay = NeedsCalculator.calcFatPerDay(currentSettings)
         } else {
             currentSettings.caloriesPerDay = (changedSettings.proteinPerDay * 4.1).toInt()
             currentSettings.caloriesPerDay += (changedSettings.carbohydratesPerDay * 4.1).toInt()
@@ -157,9 +108,9 @@ class SettingsRepository(localStorage: LocalStorageInterface) {
     }
 
     fun firstStart() {
-        val settignsEntity = getSettings()
-        settignsEntity.waterPerDay = 2.0F
-        storeSettings(settignsEntity)
+        val settingsEntity = getSettings()
+        settingsEntity.waterPerDay = 2.0F
+        storeSettings(settingsEntity)
     }
 
 
